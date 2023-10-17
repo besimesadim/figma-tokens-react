16a4ee85e67935370880d7dea0b47f603a433079
Besim Esadi <besimesadi@gmail.com>
2023-10-17 14:18:16 +0200

    add token colors

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import sass from "sass";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/figma-tokens-react/",
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
 });
